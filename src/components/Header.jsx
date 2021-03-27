8ac94731a96df7e90b5dec834cb05d378f3cf06d
HolyFire-art <80785289+HolyFire-art@users.noreply.github.com>
2021-03-27 22:50:07 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.freelogodesign.org/Content/img/logo-colors-meanings/logos-oranges/logo-fld04.svg' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://i.pinimg.com/originals/14/d0/80/14d08067a392a16a330f787d980bebaa.jpg' />
+      </div>
+      <div>
+        avatar + description
+        </div>
+      <div>
+        My posts
+          <div>
+          New post
+          </div>
+        <div>
+          <div>
+            post 1
+            </div>
+          <div>
+            post 2
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
